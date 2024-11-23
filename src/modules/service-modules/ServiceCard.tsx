b6fc278d3fd0c92ebde87703b86f3b4fe2f18753
jaimeenkoffeekodes <149799052+jaimeenkoffeekodes@users.noreply.github.com>
2024-11-23 15:39:36 +0530

    fine tuning : 2

--- src/modules/service-modules/ServiceCard.tsx
+++ src/modules/service-modules/ServiceCard.tsx
@@ -9,18 +9,16 @@ interface ServiceType {
 
 const ServiceCard = ({ service }: { service: ServiceType }) => {
   return (
-    <>
-      <div className="gradient-card-mask  max-[375px]:!w-full sm:w-full lg:w-[320px] text-[#CAD1E9] !p-5">
-        <div className="size-[40px]">
-          <Image src={service.icon} alt="" width={100} height={30} />
-        </div>
-        <h3 className="text-[24px] font-medium mt-2">{service.title}</h3>
-        <p className="text-[18px] !font-thin my-3">
-          {service.description}
-        </p>
-        <button className="text-white sora">Explore More ➡</button>
-      </div >
-    </>
+    <div className="bg-[url('/serviceCardBg.svg')]  bg-contain bg-no-repeat min-h-[300px]  w-[320px] text-[#CAD1E9] !p-5 ">
+      <div className="size-[40px]">
+        <Image src={service.icon} alt="" width={100} height={30} />
+      </div>
+      <h3 className="text-[20px]  font-medium mt-2">{service.title}</h3>
+      <p className="text-[15px]  !font-thin my-3">
+        {service.description}
+      </p>
+      <button className="text-white sora">Explore More ➡</button>
+    </div >
   );
 };
 
